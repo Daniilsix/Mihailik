6efb3ddf4926cbcc8656cb1551be302d84a48a52
Daniilsix <104648980+Daniilsix@users.noreply.github.com>
2022-04-29 21:42:14 +0300

    добавил файл index.php

---
+++ index.php
@@ -0,0 +1,269 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Дима это наш сайт </title>
+<meta charset="utf-8">
+<meta name="description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux, macOS and FreeBSD. The purpose of this project is to accurately emulate the PlayStation 3 in its entirety with the power of reverse engineering and community collaboration.">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, macos, freebsd, open source, nekotekina, kd11, home">
+<meta name="google-site-verification" content="cO1o6sx54cvKxhbnYsABWtl4sYFj9uVKV0DxLKZkWv8"/>
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="RPCS3 - The PlayStation 3 Emulator" />
+<meta property="og:description" content="Дима мы все хотим чтоб ты стал меньше агресивным" />
+<meta property="og:image" content="https://rpcs3.net/img/meta/mobile/1200.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:url" content="https://rpcs3.net" />
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="М" />
+
+<meta name="twitter:title" content="RPCS3 - The PlayStation 3 Emulator">
+<meta name="twitter:description" content="RPCS3 is a multi-platform open-source Sony PlayStation 3 emulator and debugger written in C++ for Windows, Linux, macOS and FreeBSD made possible with the power of reverse engineering.">
+<meta name="twitter:image" content="https://rpcs3.net/img/meta/mobile/1200.png">
+<meta name="twitter:site" content="@rpcs3">
+<meta name="twitter:creator" content="@rpcs3">
+<meta name="twitter:card" content="summary_large_image">
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+<script data-cfasync="false" src='/lib/js/carousel.js?v=1'></script>
+<body>
+
+</head>
+<?php include 'lib/module/sys-php.php';?>
+<?php
+?>
+<div class="page-con-content landing-con-override">
+	<div class="wavebar-con-container-master">
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+	<div class='landing-ico-scrolldown'>
+	</div>
+	<div class="landing-con-main">
+		<div id="object-particles">
+		</div>
+		<div class="content-con-backdrop darkmode-header">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class='landing-con-left content-expand '>
+					<div class="landing-con-container scale-content-txt-1 fade-in-onload">
+						<div class='landing-ico-logo'>
+						</div>
+						<div class='landing-tx1-heading pulsate'>
+							<h1>ЭТОТ САЙТ ПОСВЕЩЯЕЬСЯ ДИМЕ МИХАЙЛИКУ<br>
+							 </h1>
+						</div>
+						<div class='landing-tx2-heading'>
+							<p>
+								 Дима выбирай мы(твои друзья) или Германия
+							</p>
+							<div class='landing-btn-container'>
+								<div class="build-btn-button">
+									<div class="build-ico-button">
+										<div class="build-ico-os">
+											<img alt="german" src="img/icons/os/german.png" style='height: 100%; width: 100%; object-fit: contain'/>
+									        <img alt="jgh"   src="img/icons/os/jgh.png" style='height: 100%; width: 100%; object-fit: contain; display:none;'/>
+											<img alt="nbx"   src="img/icons/os/nbx.png" style='height: 100%; width: 100%; object-fit: contain; display:none;'/>
+											<img alt="box" src="img/icons/os/box.png" style='height: 100%; width: 100%; object-fit: contain; display:none;'/>
+										</div>
+									</div>
+									<a href="https://www.google.com/search?q=%D0%B3%D0%B5%D1%80%D0%BC%D0%B0%D0%BD%D0%B8%D1%8F&sxsrf=APq-WBvPBX00uEnw_Xo6U5HS747XHZ8Rew:1650528334467&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj19sSY2aT3AhVPtKQKHSq-D4EQ_AUoAnoECAIQBA&biw=1536&bih=696&dpr=1.25"target="_blank">
+									<div class="build-tx1-button">
+										<span>Дима</span>
+										<span class="build-tx2-label">лох</span>
+									</div>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='landing-con-right content-remove '>
+					<div class='landing-img-hero' style="background: url(/img/graphics/landing/hero-main.png) no-repeat center; background-size: contain;">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-search darkmode-slimbar">
+		<div class="search-ovr-container">
+		</div>
+		<div class='search-con-container'>
+			<div class="search-con-outer">
+				<div class="search-con-inner">
+					<div class="search-inp-search darkmode-search-border">
+						<div class="search-ico-search darkmode-invert">
+						</div>
+						<div class="search-con-author darkmode-txt">
+							<a>
+							<span class="darkmode-txt">Сделано <span class="search-img-author"></span>
+							<span class="search-tx1-author">нами</span>
+							</span>
+							</a>
+						</div>
+						<form action='https://www.instagram.com/_dimamihailik/' method='get'>
+							<input class="database-search darkmode-txt darkmode-search-bg" name="g" placeholder="Введите текст">
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="landing-con-discord darkmode-discord">
+		<div class="discord-left-logo">
+			<img alt="Discord Left Banner" src="/img/graphics/svg/discord-left.svg" style="width: 100%; height: 100%;"style="border-radius: 30px;">
+		</div>
+		<div class="discord-center-logo">
+			<img alt="Discord Logo" src="/img/graphics/svg/discord-center.svg" style="width: 100%; height: 100%;" style="border-radius: 30px;">
+		</div>
+		
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="discord-con-container">
+					<div class="discord-con-logo"style="border-radius: 30px;">
+						<div class="discord-img-logo"style="border-radius: 30px;">
+						</div>
+					</div>
+					<div class="discord-con-mantra">
+						<div class="discord-wrp-mantra scale-content-txt-1">
+							<div class="discord-tx1-mantra darkmode-txt">
+								<h2>Дима обращение от Ярика</h2>
+								<div class="landing-con-divider">
+								</div>
+							</div>
+							<div class="discord-tx2-mantra darkmode-txt">
+								<p>
+									 Привет Дима,ты уже вышел из уборной?Прошу тебя больше не ставь смайлики себе на лицо,на фотках.
+									 Are you fucking slave .
+								</p>
+							</div>
+							<a href="https://standoff2.ru/" target="_blank">
+							<div class="discord-btn-button">
+								<div class="discord-ico-button">
+								</div>
+								<div class="discord-tx1-button">
+									<span>не меняй нас</span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<svg class="discord-con-animate" preserveaspectratio="none" width="1920" height="100%" viewbox="0 0 1920 330">
+		<path class="discord-svg-animate" fill="#fff" fill-opacity="0.1" d="M140.881198,194.260295 C257.600568,129.32862 342.939626,119.84993 418.009939,203.154617 C493.080251,286.459305 545.728689,70.9046172 636.439626,63.9593047 C727.150564,57.0139922 768.99822,139.670242 858.802907,119.431961 C948.607595,99.1936797 1071.91228,-32.9977266 1243.91228,7.75227342 C1415.91228,48.5022734 1404.10369,208.584305 1508.27166,178.709305 C1612.43963,148.834305 1633.73291,79.913472 1711.63588,98.8569055 C1776.28676,114.577866 1819.96778,221.391836 1889.37253,185.808108 C2017.32661,120.206212 2004.01952,336.769569 2004.01952,336.769569 L271.635881,337 L-149.063338,337 C-149.063338,337 -245.850307,175.637635 -58.0633382,228.867188 C33.8652851,254.92501 64.1722713,236.933925 140.881198,194.260295 Z"></path>
+		</svg>
+	</div>
+	<div class="landing-con-adsense">
+		<div class='adsense-tx1-description darkmode-txt'>
+			<span>Дима как ты замметил мы эволюцианировали в развитии </span>
+		</div>
+		<div class="adsense-con-container darkmode-slimbar div-css-board-ad">
+			<div class="adsense-con-frame">
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
+						</script>
+				<ins class="adsense-con-insert adsbygoogle" data-ad-client="ca-pub-9076246674760451" data-ad-slot="1835527222"></ins>
+				<script>
+						(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-contribute darkmode-github">
+		<div class="github-img-backdrop">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="github-con-container">
+					<div class="github-con-logo">
+						<div class="github-img-logo">
+						</div>
+					</div>
+					<div class="github-con-mantra">
+						<div class="github-wrp-mantra scale-content-txt-1 ">
+							<div class="github-tx1-mantra darkmode-txt">
+								<h2>Обращение от нас</h2>
+								<div class="landing-con-divider">
+								</div>
+							</div>
+							<div class="github-tx2-mantra darkmode-txt">
+								<p>
+									 Дима какого черта ты постоянно от нас морозишся,как только мы тебе звоним ты либо не отвечаешь или говоришь с нами в грубой форме речи,Дим нам это не нравиться
+								</p>
+							</div>
+							<a href="https://www.google.com/search?q=pubg+mobile&sxsrf=APq-WBu0fhijEZwlRdSkVWpL45lIJmLSng%3A1650526212622&ei=BAhhYqTGJebm1sQPn7aCoAM&gs_ssp=eJzj4tVP1zc0zDA3LUkuyMoxYPTiLihNSlfIzU_KzEkFAHoyCPw&oq=pubg+&gs_lcp=Cgdnd3Mtd2l6EAEYAzIECCMQJzIECAAQQzINCC4QgAQQhwIQsQMQFDILCC4QgAQQsQMQ1AIyCwguEIAEELEDENQCMgUILhCABDIKCAAQgAQQhwIQFDIFCAAQgAQyCwgAEIAEELEDEIMBMggIABCABBCxAzoHCCMQsAMQJzoHCAAQRxCwAzoPCC4Q1AIQyAMQsAMQQxgBOgcIABCABBAKSgQIQRgASgQIRhgAULQHWNALYK8baAFwAXgAgAGGAYgBhQOSAQMzLjGYAQCgAQHIAQzAAQHaAQQIARgI&sclient=gws-wiz" target="_blank">
+							<div class="github-btn-button">
+								
+								<div class="github-tx1-button">
+									<span>ты променял нас на это?</span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-adsense">
+		<div class='adsense-tx1-description darkmode-txt'>
+			<span>и создали для тебя сайт:)</span>
+		</div>
+		<div class="adsense-con-container darkmode-slimbar div-css-board-ad">
+			<div class="adsense-con-frame">
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
+						</script>
+				<ins class="adsense-con-insert adsbygoogle" data-ad-client="ca-pub-9076246674760451" data-ad-slot="1835527222"></ins>
+				<script>
+						(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
+			</div>
+		</div>
+	</div>
+	<div class="landing-con-patreon darkmode-feature">
+		<div class="patreon-img-backdrop">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="patreon-con-container">
+					<div class="patreon-con-logo">
+						<div class="patreon-img-logo">
+						</div>
+					</div>
+					<div class="patreon-con-mantra">
+						<div class="hero-patreon-wrp-mantra scale-content-txt-1 ">
+							<div class="patreon-tx1-mantra">
+								<h2>Обращение на последок</h2>
+								<div class="landing-con-divider" style="background: #ff424d">
+								</div>
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt">
+								<p>
+									 Дима надеюсь ты нас не забыл,мы все еще с тобой дружем 
+								</p>
+							</div>
+							<a href="https://www.instagram.com/g_roov_e.boy/" target="_blank">
+							<div class="patreon-btn-button">
+								<div class="patreoт-ico-button">
+								</div>
+								<div class="patreon-tx1-button">
+									<span>Понравился сайт?</span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
